ace9ae1342b9847296c7326c4ae0a7fc19b5400a
jks6404 <jannusingh.75@gmail.com>
2024-03-15 10:51:51 +0530

    first commit

---
+++ src/component/Hero.js
@@ -0,0 +1,22 @@
+
+export default function Hero(){
+    return(
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>YOUR PiZZa PARTY NEEDS SOMETHING SPECIAL</h1>
+                <p>WE ARE PROVIDING BEST OUT OFBEST PIZZA TO OUR CUSTOMER AND THEY LOVING IT.</p>
+            
+            <div className="hero-btn">
+                <button>Order now</button>
+                <button className="sec-btn">More</button>
+            </div>
+            <div className="food-app">
+                <p>Also Available On Zomato,Swiggy,Uber Eats.</p>
+            </div>
+            </div>
+            <div className="hero-img">
+         <img src="piza.avif" alt="pizza" />
+            </div>
+        </main>
+    )
+}
---
+++ src/component/Navigation.jsx
@@ -0,0 +1,22 @@
+
+
+const Navigation=()=>{
+    return(
+        <div>
+          <nav>
+        <div  className="logo">
+          <img src="brand-logo.jpg" alt="logo"/>
+        </div>
+        <ul>
+          <li href="#">Home</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button id="login" type="login">Login</button>
+        </nav>
+        </div>
+      )
+}
+
+export default Navigation;
